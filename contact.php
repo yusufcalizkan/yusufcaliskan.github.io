00b403dd55e4f4c849bc74920de0ff7b25e4b650
Yusuf Caliskan <yusufcaliskan2004@gmail.com>
2023-09-22 20:35:37 +0300

    v2.0
    Completely changed the template

---
+++ contact.php
@@ -0,0 +1,32 @@
+<?php
+$field_name = $_POST['name'];
+$field_email = $_POST['email'];
+$field_message = $_POST['message'];
+
+$mail_to = 'yusufcaliskan2004@gmail.com';
+$subject = 'Message from a site visitor '.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message sent.');
+		window.location = 'contact_page.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Message failed. Please, send an email to yusufcaliskan2004@gmail.com');
+		window.location = 'contact_page.html';
+	</script>
+<?php
+}
+?>
