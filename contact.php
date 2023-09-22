a32232c3dea0aa5364a507baf532e189446a0d88
Yusuf Caliskan <yusufcaliskan2004@gmail.com>
2023-09-22 21:47:24 +0300

    b

--- contact.php
+++ contact.php
@@ -1,32 +1,17 @@
 <?php
-$field_name = $_POST['name'];
-$field_email = $_POST['email'];
-$field_message = $_POST['message'];
 
-$mail_to = 'yusufcaliskan2004@gmail.com';
-$subject = 'Message from a site visitor '.$field_name;
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$mailFrom = $_POST['email'];
+	$message = $_POST['message'];
 
-$body_message = 'From: '.$field_name."\n";
-$body_message .= 'E-mail: '.$field_email."\n";
-$body_message .= 'Message: '.$field_message;
+	$mailTo = "yusufcaliskan2004@gmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have recieved an e-mail from ".$name.".\n\n".$message;
 
-$headers = 'From: '.$field_email."\r\n";
-$headers .= 'Reply-To: '.$field_email."\r\n";
-
-$mail_status = mail($mail_to, $subject, $body_message, $headers);
-
-if ($mail_status) { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Message sent.');
-		window.location = 'contact_page.html';
-	</script>
-<?php
+	mail($mailTo, $txt, $headers);
+	header("Locatiın: index.html?mailsend");
 }
-else { ?>
-	<script language="javascript" type="text/javascript">
-		alert('Message failed. Please, send an email to yusufcaliskan2004@gmail.com');
-		window.location = 'contact_page.html';
-	</script>
-<?php
-}
-?>
+
+
+
